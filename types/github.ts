743e3c6ab76298fa0cf4a82161d8003dcc7fe31f
NiftyAndy <andy@niftyleague.com>
2026-09-25 04:14:38 -0400

    chore: remove dead code from Card component and GitHubRepo type

--- types/github.ts
+++ types/github.ts
@@ -1,14 +1,11 @@
 export interface GitHubRepo {
-  id: number
   name: string
   description: string | null
   html_url: string
   homepage: string | null
   topics: string[]
-  language: string | null
   stargazers_count: number
   forks_count: number
-  updated_at: string
 }
 
 export interface PinnedRepo {
